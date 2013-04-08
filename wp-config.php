4fe7fd8a3a5ae8af60b2d5b0e9d0d1a33ab3b3ad
Samuel Molinski <sjmolinski@gmail.com>
2013-04-08 00:40:13 -0300

    + multisite settings

--- wp-config.php
+++ wp-config.php
@@ -40,7 +40,14 @@ define('DB_COLLATE', '');
 define('WP_ALLOW_MULTISITE', true);
 
 /* Multisite */
-define('WP_ALLOW_MULTISITE', true);
+//define('WP_ALLOW_MULTISITE', true);
+
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'nissenidea.com');
+define('PATH_CURRENT_SITE', '/smt/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 
 /**#@+
  * Authentication Unique Keys and Salts.
