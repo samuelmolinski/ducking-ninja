ca15ec2a3bc6e6be22bf4c6e785a33292a7a6dda
Samuel Molinski <sjmolinski@gmail.com>
2013-06-18 15:45:17 -0300

    + added download.php - download shortcode plugin

---
+++ download.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+$file = $_GET['file'];
+
+
+
+header('Content-Description: File Transfer');
+
+header('Content-Type: application/octet-stream');
+
+header('Content-Disposition: attachment; filename='.basename($file));
+
+header('Content-Transfer-Encoding: binary');
+
+header('Expires: 0');
+
+header('Cache-Control: public'); //for i.e.
+
+header('Pragma: public');
+
+
+
+ob_clean();
+
+flush();
+
+readfile($file);
+
+exit;
